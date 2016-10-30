0c0729c6818da3a210d4d6c53fb96863d4fb6085
Nazarii <counter.sparta@gmail.com>
2016-10-30 21:16:25 +0200

    Script to be executed directly only.

--- py_script.py
+++ py_script.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import os
-value1=raw_input('Enter path: ')
-os.chdir(value1)
-value2 =raw_input('Enter dir name: ')
-value3 =raw_input('Enter NEW name: ')
-os.rename(value2,value3)
-print 'Complete'
+
+def main():
+    value1=raw_input('Enter path: ')
+    os.chdir(value1)
+    value2 =raw_input('Enter dir name: ')
+    value3 =raw_input('Enter NEW name: ')
+    os.rename(value2,value3)
+    print('Complete')
+
+if __name__ == '__main__':
+    main()
